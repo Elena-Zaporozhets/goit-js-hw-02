bcdad089bb0c724ad67da480d98ee370d8701271
Elena-Zaporozhets <elenazaporozhec28@gmail.com>
2025-12-29 23:15:09 +0200

    task-2

--- js/task-2.js
+++ js/task-2.js
@@ -1,10 +1,9 @@
 function formatMessage(message, maxLength) {
-    if (message.maxLength <= maxLength) {
+    if (message.length <= maxLength) {
         return message; 
     }
-     {
         return message.slice(0, maxLength) + "...";
-    }
+    
 }
 
 
